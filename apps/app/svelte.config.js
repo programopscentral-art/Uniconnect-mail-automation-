ef203e68dfd940528535ff2ed7b0538096880273
Karthikeya <Karthikeya.a054@gmail.com>
2026-02-26 16:36:50 +0530

    fix: move bodySizeLimit to adapter config

--- apps/app/svelte.config.js
+++ apps/app/svelte.config.js
@@ -11,8 +11,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		bodySizeLimit: 50 * 1024 * 1024 // 50MB
+		adapter: adapter({
+			bodySizeLimit: 50 * 1024 * 1024 // 50MB
+		})
 	}
 };
 
